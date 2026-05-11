18403f55e64a2bccff1c0d19972fbc10585a3519
netra <netrahun47@gmail.com>
2026-05-11 13:15:17 -0400

    changed vite

--- src/App.jsx
+++ src/App.jsx
@@ -140,7 +140,7 @@ function App() {
 
         <motion.video variants={videoPop(index)} transition={{ type: "spring", duration: 1 }} controls="true">
         <motion.source
-          src={"/videos/" + game.videosrc}
+          src={"/Netras-Games/videos/" + game.videosrc}
           type="video/mp4"
         ></motion.source>
       </motion.video>
